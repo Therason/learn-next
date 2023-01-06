92a87d9f3c6ffe53cf6fbddcdd0e448f78e43b89
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-06 12:18:04 -0500

    Can't help myself

--- pages/prezzy/slide3.tsx
+++ pages/prezzy/slide3.tsx
@@ -5,9 +5,9 @@ export default function Slide3(){
     <div className="content">
       <h1>What does Next.js do to improve this? 📝</h1>
       <ul>
-        <li>Provides optimized HTML components</li>
-        <li>Page and API routing</li>
-        <li>Pre-rendered DOM and hydration by default</li>
+        <li>Provides optimized HTML components 💪</li>
+        <li>Page and API routing 🔎</li>
+        <li>Pre-rendered DOM and hydration by default 🌊</li>
         <li>Almost everything is in one place!</li>
       </ul>
       <div className="left"><Link href="/prezzy/slide2">&lt;-</Link></div>
