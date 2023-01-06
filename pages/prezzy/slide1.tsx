0bca9ea97f4bf4572801f7e59340cf4ce45cd4b2
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-06 12:14:51 -0500

    Add links

--- pages/prezzy/slide1.tsx
+++ pages/prezzy/slide1.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+// all of these slides are statically generated at build time!
 export default function Slide1(){
   return (
     <div className="content">
-      <h1>What is <Image className="svg" src="/next.svg" height="32" width="175" alt="" />? ✨</h1>
+      <h1>What is <Image className="svg" src="/next.svg" height="32" width="175" alt="Next.js logo" />? ✨</h1>
       <ul>
         <li>React framework ⁉️</li>
         <li>Performance oriented 🤔</li>
--- pages/prezzy/slide8.tsx
+++ pages/prezzy/slide8.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
+import LinkLogo from '../../components/LinkLogo'
 
 export default function Slide8(){
   return (
     <div className="content">
       <h1>Other things I like... 🤡</h1>
       <ul>
-        <li>ISR to mix SSG and SSR 😱</li>
+        <li><Link href="https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration" target="_blank">ISR <LinkLogo /></Link> to mix SSG and SSR 😱</li>
         <li>AMAZING docs!!!! 🤓</li>
         <li>TypeScript support 💘</li>
         <li>Super easy deployment with Vercel 🚀</li>
         <li>Even easier setup with <span className="code">create-next-app</span> 🖥️</li>
-        <li>Templates for anything you can think of 💭</li>
+        <li><Link href="https://vercel.com/templates" target="_blank">Templates <LinkLogo /></Link> for anything you can think of 💭</li>
       </ul>
       <div className="left"><Link href="/prezzy/slide7">&lt;-</Link></div>
       <div className="right"><Link href="/prezzy/slide9">-&gt;</Link></div>
